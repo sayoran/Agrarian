9ab0ef01052383d5b40b78cb982b430f55f44e25
Michelle Neudörfl <mneudoerfl@techfak.uni-bielefeld.de>
2017-09-08 22:53:42 +0200

    clipper is a tool

--- src/main/java/de/sayoran/agrarian/item/ModItems.java
+++ src/main/java/de/sayoran/agrarian/item/ModItems.java
@@ -1,5 +1,6 @@
 package de.sayoran.agrarian.item;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
@@ -7,7 +8,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModItems {
 
     public static ItemBase cropSticks = new ItemBase("crop_sticks");
-    public static ItemBase clipper = new ItemBase("clipper");
+    public static ItemBase clipper = new ItemBase("clipper").setCreativeTab(CreativeTabs.TOOLS);
     public static ItemBase journal = new ItemBase("journal");
     public static ItemBase magnifyingGlass = new ItemBase("magnifying_glass");
     public static ItemBase trowel = new ItemBase("trowel");
