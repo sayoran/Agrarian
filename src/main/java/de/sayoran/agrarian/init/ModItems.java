aae33cad8d96c399405471649febdd2939316691
sayoran <mail@sayoran.de>
2017-09-10 03:16:44 +0200

    fixed init

--- src/main/java/de/sayoran/agrarian/init/ModItems.java
+++ src/main/java/de/sayoran/agrarian/init/ModItems.java
@@ -1,8 +1,12 @@
-package de.sayoran.agrarian.item;
+package de.sayoran.agrarian.init;
 
+import de.sayoran.agrarian.item.ItemBase;
+import de.sayoran.agrarian.item.ItemModFood;
+import de.sayoran.agrarian.item.ItemModSeed;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
+
 
 
 public class ModItems {
@@ -12,7 +16,7 @@ public class ModItems {
     public static ItemBase journal = new ItemBase("journal", 1);
     public static ItemBase magnifyingGlass = new ItemBase("magnifying_glass", 1);
     public static ItemBase trowel = new ItemBase("trowel", 1);
-    public static ItemCornSeed cornSeed = new ItemCornSeed();
+    public static ItemModSeed cornSeed = new ItemModSeed("corn_seed");
     public static ItemModFood corn = new ItemModFood("corn", 3, 0.6f,false,64);
     public static ItemModFood poppedCorn = new ItemModFood("popped_corn", 4, 0.6f,false, 32);
 
